a15b1627eab86ad64c95beb9dda6c8c95320ff76
Anwaydeep Nath <81034448+Anwaydeep2000@users.noreply.github.com>
2021-07-04 12:12:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+let weather = {
+    apikey: "345c1a7ed5eb50c5db2f4a2b683e041b",
+    fetchweather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+            + city + "&units=metric&appid="
+            + this.apikey)
+            .then((Response) => Response.json())
+            .then((data) => this.showweather(data));
+    },
+    showweather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        // console.log(name, icon, description, temp, humidity, speed)
+        document.querySelector(".city").innerText = name;
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText =
+            "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+            "Wind Speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search:
+        function () {
+            this.fetchweather(document.querySelector(".search_bar").value);
+        }
+};
+
+document.querySelector(".search button")
+    .addEventListener("click", function () {
+        weather.search();
+    });
+
+document.querySelector(".search_bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
+weather.fetchweather("Bengaluru")
